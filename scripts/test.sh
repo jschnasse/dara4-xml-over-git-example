2271bb362946b49796102d12361632cfe9423586
Jan Schnasse <jan.schnasse@gesis.org>
2024-06-21 10:51:30 +0200

    add gitlab xml validating

---
+++ scripts/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function test {
+result=0;
+for i in $(ls data);
+do  
+echo -e "\n-------------------\n"; 
+echo -e "$i\n\n"; 
+xmllint --noout --schema http://www.da-ra.de/dara/schemadefinitions/dara.xsd  data/$i; 
+ret=$?
+
+if [ ! $ret -eq 0 ]
+then
+result=$ret;
+fi
+
+echo -e "-------------------\n";
+done
+return $result;
+}
+
+test
